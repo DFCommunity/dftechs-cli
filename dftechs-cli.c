8ce7fcd2ee6e08f0b70f1e8aecd42325aec31a63
BRAVO68WEB <bravo68web@gmail.com>
2020-10-19 11:38:48 +0530

    Project Initiated

---
+++ dftechs-cli.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int initProject(){
+    printf("Work in Progress \n");
+}
+int main() 
+{ 
+	printf("Initiated DF Techs CLI ....\n");
+    
+    printf("Initiation Completed .............\n \n");
+
+    int mainMenuOption;
+
+    printf("Enter your Choice :- \n");
+    scanf("%d", &mainMenuOption);
+
+    // true if number is less than 0
+    if (mainMenuOption=='1') {
+        initProject();
+    }
+    else if (mainMenuOption=='2'){
+        printf("Work in Progress \n");
+        main();
+    }
+    else if (mainMenuOption=='3'){
+        printf("Work in Progress \n");
+        main();
+    }
+    else if (mainMenuOption=='4'){
+        printf("Work in Progress \n");
+        main();
+    }
+    else{
+        printf("Invalid Choice .... \n\n Exiting DF Techs CLI\n");
+        exit(1);
+    }
+    return 0; 
+} 
